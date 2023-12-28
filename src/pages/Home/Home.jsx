612c5a90140d7e65856d9d59dc744d53678ffdb6
Karoline Diniz Ramos <dinizkaroline923@gmail.com>
2023-12-28 11:50:52 -0300

    :lipstick: refactor: estiliza home.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,30 @@
-import { useEffect, useState } from "react";
 import { Menu } from "@comp/ui/Menu";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Unstable_Grid2";
+import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import imgHome from "@img/img-home.jpg";
 
 const Home = () => {
   return (
-    <Grid component="main" rowSpacing={12}>
-      <Grid component="header" mt={3} mb={3}>
+    <Grid container spacing={4} justifyContent="center">
+      <Grid item xs={12}>
         <Menu />
       </Grid>
-          </Grid>
+
+      <Grid item xs={12}>
+       <Typography variant="h4" align="left" gutterBottom style={{ color: "#6357F1",  marginLeft:"12rem", marginTop:"12rem" }}>
+          Welcome to Our Virtual Market
+        </Typography>
+        <Typography variant="body1" align="left" paragraph style={{ lineHeight: 1.6,  marginLeft:"12rem" }}>
+          Explore a wide variety of products and shop online with us.
+        </Typography>
+
+        <img
+          src={imgHome}  
+          alt="Market Image"
+          style={{ maxWidth: "40%", height: "auto", marginTop: "-19rem",justifyContent: "right", alignItems: "center", display: "flex", marginLeft: "53rem"}}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
