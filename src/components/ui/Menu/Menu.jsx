ca45fd3638cf5308d6a74d98a2b8a1f0484d7ace
Karoline Diniz Ramos <dinizkaroline923@gmail.com>
2023-12-28 11:49:31 -0300

    :sparkles: feat: implementa o menu

--- src/components/ui/Menu/Menu.jsx
+++ src/components/ui/Menu/Menu.jsx
@@ -1,6 +1,7 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { Link } from "react-router-dom";
 
 import Logo from "@img/ecommerce_10185772.png";
 import products from "@img/box_6452366.png";
@@ -9,6 +10,47 @@ import like from "@img/like_6452430.png";
 import notificacaoIcone from "@img/notificacao-icone.png";
 import configuracoesIcone from "@img/configuracoes-icone.png";
 import usuarioIcone from "@img/usuario-icone.png";
+
+const MenuItem = ({ to, icon, label }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <Box
+      component={Link}
+      to={to}
+      sx={{
+        width: 106,
+        height: 93.23,
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        gap: 1,
+        display: "flex",
+        textDecoration: "none",
+        transition: "transform 0.3s ease-in-out, color 0.3s",
+        transform: isHovered ? "scale(1.15)" : "scale(1)",
+        color: isHovered ? "#000" : "#6357F1",
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img style={{ width: 64, height: 64 }} src={icon} alt={label} />
+      <Typography
+        sx={{
+          width: 106,
+          height: 24.23,
+          textAlign: "center",
+          fontSize: 14,
+          fontFamily: "Inter",
+          fontWeight: "600",
+          wordWrap: "break-word",
+        }}
+      >
+        {label}
+      </Typography>
+    </Box>
+  );
+};
 
 const Menu = () => {
   return (
@@ -30,11 +72,7 @@ const Menu = () => {
           height: 66.67,
         }}
       >
-        <img
-          style={{ width: 100, height: 100 }}
-          src={Logo}
-          alt="Logo"
-        />
+        <img style={{ width: 100, height: 100 }} src={Logo} alt="Logo" />
       </Box>
       <Box
         sx={{
@@ -45,112 +83,9 @@ const Menu = () => {
           display: "inline-flex",
         }}
       >
-
-<Box
-          component={Link}
-          to={"/Home"}
-          sx={{
-            width: 106,
-            height: 92,
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            gap: 1,
-            display: "inline-flex",
-            textDecoration: "none",
-          }}
-        >
-          <img
-            style={{ width: 64, height: 64 }}
-            src={home}
-            alt="Ícone para Home"
-          />
-          <Typography
-            sx={{
-              width: 106,
-              height: 23,
-              textAlign: "center",
-              color: "#6357F1",
-              fontSize: 14,
-              fontFamily: "Inter",
-              fontWeight: "600",
-              wordWrap: "break-word",
-            }}
-          >
-            HOME
-          </Typography>
-        </Box>
-
-        <Box
-          component={Link}
-          to={"/produtos"}
-          sx={{
-            width: 106,
-            height: 93.23,
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            gap: 1,
-            display: "flex",
-            textDecoration: "none",
-          }}
-        >
-          <img
-            style={{ width: 64, height: 64 }}
-            src={products}
-            alt="products"
-          />
-          <Typography
-            sx={{
-              width: 106,
-              height: 24.23,
-              textAlign: "center",
-              color: "#6357F1",
-              fontSize: 14,
-              fontFamily: "Inter",
-              fontWeight: "600",
-              wordWrap: "break-word",
-            }}
-          >
-            PRODUTOS
-          </Typography>
-        </Box>
-
-        <Box
-          component={Link}
-          to={"/project"}
-          sx={{
-            width: 106,
-            height: 93.23,
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            gap: 1,
-            display: "flex",
-            textDecoration: "none",
-          }}
-        >
-          <img
-            style={{ width: 64, height: 64 }}
-            src={like}
-            alt="informações do projeto"
-          />
-          <Typography
-            sx={{
-              width: 106,
-              height: 24.23,
-              textAlign: "center",
-              color: "#6357F1",
-              fontSize: 14,
-              fontFamily: "Inter",
-              fontWeight: "600",
-              wordWrap: "break-word",
-            }}
-          >
-            PROJECT
-          </Typography>
-        </Box>
-
+        <MenuItem to={"/Home"} icon={home} label={"HOME"} />
+        <MenuItem to={"/produtos"} icon={products} label={"PRODUTOS"} />
+        <MenuItem to={"/project"} icon={like} label={"PROJECT"} />
       </Box>
       <Box
         sx={{
