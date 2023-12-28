1fa12868b0bcbf87827d26033f538b9d25a5a3c4
Karoline Diniz Ramos <dinizkaroline923@gmail.com>
2023-12-28 14:18:26 -0300

    :lipstick: refactor: estiliza parte dos créditos

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,12 +1,23 @@
+import React from "react";
 import { Menu } from "@ui/Menu";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const Project = () => {
-
   return (
     <Grid container spacing={4} justifyContent="center">
       <Grid item xs={12}>
         <Menu />
+      </Grid>
+
+      <Grid item xs={12} style={{ marginTop: "12rem" }}>
+        <Typography variant="h5" align="center">
+          Project Credits
+        </Typography>
+        <Typography variant="body1" align="center">
+        <br />This project was developed as part of a technical challenge for a front-end position. <br />
+          For more information, please refer to the README in the repository on GitHub with the link: <a href="https://github.com/KarolDiniz/elevaty-store#" target="_blank" rel="noopener noreferrer">https://github.com/KarolDiniz/elevaty-store#</a>
+        </Typography>
       </Grid>
     </Grid>
   );
